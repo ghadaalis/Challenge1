ca4f805b82e4ff5c7f2049bc8cf7ef4fa0b8cad2
Ghada <ghada-alshabanat@hotmail.com>
2024-10-03 17:11:07 +0300

    end page v2

--- Challenge1/EndPage.swift
+++ Challenge1/EndPage.swift
@@ -14,21 +14,64 @@ struct EndPage: View {
                 .resizable()
                 .aspectRatio(contentMode: .fill)
             Text ("انظر الى مأنجزته خلال فترة التركيز!")
+                .fontWeight(.bold)
                 .multilineTextAlignment(.center)
-                .padding(.top,-200)
+                .padding(.top,-250)
                 .foregroundColor(Color("line1HomePage"))
                 .font(Font.custom("SF Arabic Rounded", size: 36))
                 .padding()
 
             Text ("استمتع بكل لحظة من إنجازاتك \nوتذكر دائماً أن كل خطوة تقربك من تحقيق أحلامك،\n استمر في العمل بجد وستصل إلى ماتريد")
-                .bold()
+                .fontWeight(.bold)
+                //.bold()
+               
                 .multilineTextAlignment(.center)
-                .padding(.top,0)
+                //.bold()
+                .padding(.top,-100)
             .foregroundColor(Color("line1HomePage"))
             .font(Font.custom("SF Arabic Rounded", size: 24))
             .padding()
             
+            //button
+            Spacer()
+            HStack{   Button(action: {
+                // Handle previous action
+            }) {
+                Text("إعادة الجولة")
+                    .font(.system(size: 18))
+                    .foregroundColor(Color("backgroundLight"))
+                    .padding()
+                    .frame(width:120,height:50)
+                    .background(Color("buttonsColor"))
+                    .cornerRadius(25)
+                    .shadow(radius: 5)
+                    .offset(x: -60, y: 300)
+            }
+                
+                Button(action: {
+                    // Handle next action
+                }) {
+                    Text("إنهاء")
+                        .font(.system(size: 18))
+                        .foregroundColor(Color("backgroundLight"))
+                        .padding()
+                        .frame(width:120,height:50)
+                        .background(Color("buttonsColor"))
+                        .cornerRadius(25)
+                        .shadow(radius: 5)
+                        .offset(x: 60, y: 300)
+                }
+                
+                
+            }
+             
+            
+            
+            
+            
+            
         }.ignoresSafeArea()
+        
             
 
 }
