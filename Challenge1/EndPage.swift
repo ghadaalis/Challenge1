b9bdd3b6b7df7661f915dfbd6dcb72425ad21701
Ghada <ghada-alshabanat@hotmail.com>
2024-10-03 16:16:13 +0300

    End page

--- Challenge1/EndPage.swift
+++ Challenge1/EndPage.swift
@@ -9,10 +9,27 @@ import SwiftUI
 
 struct EndPage: View {
     var body: some View {
-        VStack {
+        ZStack {
+            Image("EndPage")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
             Text ("انظر الى مأنجزته خلال فترة التركيز!")
-                        
-    }.ignoresSafeArea()
+                .multilineTextAlignment(.center)
+                .padding(.top,-200)
+                .foregroundColor(Color("line1HomePage"))
+                .font(Font.custom("SF Arabic Rounded", size: 36))
+                .padding()
+
+            Text ("استمتع بكل لحظة من إنجازاتك \nوتذكر دائماً أن كل خطوة تقربك من تحقيق أحلامك،\n استمر في العمل بجد وستصل إلى ماتريد")
+                .bold()
+                .multilineTextAlignment(.center)
+                .padding(.top,0)
+            .foregroundColor(Color("line1HomePage"))
+            .font(Font.custom("SF Arabic Rounded", size: 24))
+            .padding()
+            
+        }.ignoresSafeArea()
+            
 
 }
 }
