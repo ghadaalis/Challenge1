bdc5a02f2cf085a419ed4022f312f1843489ce8c
Njoud <njuood97@gmal.com>
2024-10-03 15:21:32 +0300

    last update

--- Challenge1/EndPage.swift
+++ Challenge1/EndPage.swift
@@ -9,21 +9,16 @@ import SwiftUI
 
 struct EndPage: View {
     var body: some View {
-        ZStack {
-            Image("EndPage")
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-        Text ("انظر الى مأنجزته خلال فترة التركيز!")
-                .font(.system(size:25))
-                .bold()
-                .padding()
-               
+        VStack {
+            Text ("انظر الى مأنجزته خلال فترة التركيز!")
+                        
+    }.ignoresSafeArea()
 
-        }.ignoresSafeArea()
-            
-    }
+}
 }
 
+
+
 #Preview {
-    EndPage()
+EndPage()
 }
